a413d3540212542e0d570d5eaa9be688b2dc975b
goofw <59505666+goofw@users.noreply.github.com>
2024-03-09 19:32:37 +0800

    Delete build.sh

--- build.sh
+++
@@ -1,4 +0,0 @@
-curl -fsSL -o movie-web.zip https://github.com/movie-web/movie-web/releases/latest/download/movie-web.zip
-mkdir movie-web && unzip -qd movie-web movie-web.zip && rm -f movie-web.zip
-sed -i "s|VITE_CORS_PROXY_URL:.*,|VITE_CORS_PROXY_URL: \"$CORS_PROXY_URL\",|g" movie-web/config.js
-sed -i "s|VITE_TMDB_READ_API_KEY:.*,|VITE_TMDB_READ_API_KEY: \"$TMDB_READ_API_KEY\",|g" movie-web/config.js
